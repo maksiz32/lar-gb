8abe2ee3e63c57a0efc88a6ace69b19a0dce6be8
maksiz32 <maks-manzulin@mail.ru>
2021-11-06 18:28:16 +0300

    less8. Почистил неиспользуемые зависимости

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\UserRole;
-use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
 
 class UserController extends Controller
